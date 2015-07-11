0f5ca06aaced960a8316614edd48c78da25b5f55
annProg <tecbbs@qq.com>
2015-07-12 00:57:52 +0800

    add essay

--- getpost.sh
+++ getpost.sh
@@ -28,6 +28,32 @@ function BlogSina()
 	sed -i "\$ a $url" $ofile
 }
 
-BlogSina $url
+function Douban()
+{
+	url=$1
+	start="note-header\ note-header-container"
+	end="class=\"note-ft\""
+	file=`date +%s`
+	file=$tmp/post-$file.html
+	curl -s $url -o $file
+	title=`sed -n '/<title>/,/<\/title>/p' $file |sed '1d;$d' |tr -d ' '`
+	ofile="`echo $title | iconv -f utf-8 -t gbk`.md"
+	title="# $title"
+	sed -i -n "/$start/,/$end/p" $file
+	sed -i '1d;$d' $file
+	
+	pandoc $file -o "$ofile" -t markdown_strict
+	#sed -i "1 i $title" $ofile
+	sed -i '$ a ## 原文' $ofile
+	sed -i "\$ a $url" $ofile
+}
+
+domain=`echo $url |awk -F '/' '{print $3}'`
+
+case $domain in
+	www.douban.com) Douban $url ;;
+	blog.sina.com.cn) BlogSina $url ;;
+	*) ;;
+esac
 
 rm -f $tmp/*
